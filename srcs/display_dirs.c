8d9bef93c48f99788b71b1b209b67c209f7ce33d
Kirill Naumov <knaumov@e1z2r3p44.42.us.org>
2018-10-12 14:51:26 -0700

    b_ls

---
+++ srcs/display_dirs.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   display_dirs.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: knaumov <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/01 10:42:54 by knaumov           #+#    #+#             */
+/*   Updated: 2018/10/08 17:00:23 by knaumov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "b_ls.h"
+
+static t_node			*reading_dir(t_node *path, t_flags *flags, \
+		DIR *dir, t_node *dir_list)
+{
+	struct stat			buf;
+	char				*linkname;
+	char				*str;
+	char				*str1;
+	struct dirent		*sd;
+
+	while ((sd = readdir(dir)))
+	{
+		linkname = NULL;
+		lstat(path->name, &buf);
+		if ((buf.st_mode & S_IFLNK) != S_IFLNK || ((buf.st_mode & S_IFLNK) ==\
+					S_IFLNK && flags->flag_l == 0))
+		{
+			str1 = ft_strcat(path->name, "/");
+			str = ft_strcat(str1, sd->d_name);
+			lstat(str, &buf);
+			if (flags->flag_a == 1 || sd->d_name[0] != '.')
+				dir_list = node_add(dir_list, sd->d_name, buf);
+			free(str);
+			free(str1);
+		}
+	}
+	return (dir_list);
+}
+
+void					count_dirs(t_node *path,
+	t_flags *flags, DIR *dir)
+{
+	t_node *dir_list;
+
+	dir_list = NULL;
+	dir_list = reading_dir(path, flags, dir, dir_list);
+	display_entity(path, dir_list, flags);
+	ft_list_clean(&dir_list);
+}
+
+void					display_dirs(t_node *path, t_flags *flags)
+{
+	int					cnt_dirs;
+	DIR					*dir;
+	struct stat			buf;
+
+	flags->entity_fill = 0;
+	cnt_dirs = node_size(path);
+	while (path)
+	{
+		dir = opendir(path->name);
+		if (cnt_dirs > 0 && ((path->buf.st_mode & S_IFDIR) == S_IFDIR ||\
+		((path->buf.st_mode & S_IFLNK) == S_IFLNK && flags->\
+					flag_l == 0)) && dir)
+			printf("\n") && printf("%s:\n", path->name);
+		if (!dir && !stat(path->name, &buf) && ((path->buf.st_mode & S_IFDIR) \
+		== S_IFDIR || (((path->buf.st_mode & S_IFLNK) == S_IFLNK && flags->\
+					flag_l == 0))) && (path->buf.st_mode & S_IFBLK) != S_IFBLK)
+			printf("b_ls: %s: %s\n", path->name, strerror(errno));
+		if (dir && ((path->buf.st_mode & S_IFDIR) == S_IFDIR || \
+					((path->buf.st_mode & S_IFLNK) == S_IFLNK && flags->\
+					flag_l == 0)))
+			count_dirs(path, flags, dir);
+		if (dir)
+			closedir(dir);
+		path = path->next;
+	}
+}
